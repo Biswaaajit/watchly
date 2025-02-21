d9542f8c1ea7671dbc2e80a1af76db4415ec7526
Biswajeet Barik <biswajeetbarik3936@gmail.com>
2025-02-21 22:39:28 +0530

    made some changes in likes and dislike button

--- src/Pages/VideoPage/VideoLikesAndDisLike.jsx
+++ src/Pages/VideoPage/VideoLikesAndDisLike.jsx
@@ -1,18 +1,19 @@
 import { AiOutlineDislike, AiOutlineLike } from "react-icons/ai";
 import PropTypes from "prop-types";
+import numberFormat from "../../utils/numberFormat";
 
 function VideoLikesAndDisLike({ likes, dislikes }) {
-  const modifiedLikes = (likes / 1000).toFixed(1);
-  const modifiedDislikes = (dislikes / 1000).toFixed(1);
+  const modifiedLikes = numberFormat(likes);
+  const modifiedDislikes = numberFormat(dislikes);
   return (
-    <div className="flex divide-x-2 divide-slate-300 bg-slate-200/70 w-fit px-3 py-1 rounded-full">
-      <button className="flex items-center gap-1 font-semibold pr-3">
+    <div className="flex divide-x-2 divide-slate-300 bg-slate-200/70 w-fit  rounded-full overflow-hidden">
+      <button className="flex items-center gap-1 active:bg-black active:text-white font-semibold px-3 py-1 pr-3">
         <AiOutlineLike className="text-xl" />
-        <span>{likes < 1000 ? likes : `${modifiedLikes}k`}</span>
+        <span>{modifiedLikes}</span>
       </button>
-      <button className="flex items-center font-semibold gap-1 pl-3">
+      <button className="flex items-center font-semibold active:bg-black active:text-white gap-1 pl-3 px-3 py-1">
         <AiOutlineDislike className="text-xl" />
-        <span>{dislikes < 1000 ? dislikes : `${modifiedDislikes}k`}</span>
+        <span>{modifiedDislikes}</span>
       </button>
     </div>
   );
